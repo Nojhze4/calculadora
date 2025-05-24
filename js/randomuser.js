c1fbab36b671907d2823300e0bb2f92d7348a693
Jhon Ramirez <jhramirez9716@gmail.com>
2025-05-24 17:43:10 -0500

    update css and js

---
+++ js/randomuser.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//VARIABLES
+
+const boton = document.getElementById('boton')
+const foto = document.getElementById('foto')
+const nombre = document.getElementById('nombre')
+const correo = document.getElementById('correo')
+const telefono = document.getElementById('telefono')
+
+//Funciones
+//Enlance API https://randomuser.me/api
+
+const generarUsuario = async () => {
+    const url = 'https://randomuser.me/api/';
+    const respuesta = await fetch(url);
+    const { results } = await respuesta.json();
+    const datos = results[0];
+
+    foto.src = datos.picture.medium;
+    nombre.textContent = datos.name.first;
+    correo.textContent = datos.email;
+    telefono.textContent = datos.phone;
+}
+
+//Eventos
+boton.addEventListener('click', generarUsuario);
+document.addEventListener('DOMContentLoaded', generarUsuario);
+
+
+
